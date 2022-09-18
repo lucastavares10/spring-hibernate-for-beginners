f478cbedf5e3147f6241e77a675b33edfec0f10e
Lucas Tavares <lucas_tavares10@hotmail.com>
2022-09-17 22:31:33 -0300

    Início de projetos SpringBoot, configurando aplicationContext, beans e IoC com xml

---
+++ spring-demo-one/src/com/love2code/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.love2code.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting pratice";
+	}
+	
+}
---
+++ spring-demo-one/src/com/love2code/springdemo/BasketCoach.java
@@ -0,0 +1,9 @@
+package com.love2code.springdemo;
+
+public class BasketCoach implements Coach {
+
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on hitting baskets";
+	}
+
+}
