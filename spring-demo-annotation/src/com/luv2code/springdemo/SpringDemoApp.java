c4f861a1fc6f08bd56a7e0983a5cef8c4fbe8858
Lucas Tavares <lucas_tavares10@hotmail.com>
2022-09-26 16:26:32 -0300

    Spring config with java annotations

---
+++ spring-demo-annotation/src/com/luv2code/springdemo/SpringDemoApp.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDemoApp {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+
+		System.out.println(theCoach.getDailyWorkout());
+
+		context.close();
+
+	}
+
+}
