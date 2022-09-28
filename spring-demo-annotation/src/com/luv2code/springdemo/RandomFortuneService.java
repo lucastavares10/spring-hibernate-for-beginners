ecd51e3bd87bf31398a7441926d7ffbcee43e8d3
Lucas Tavares <lucas_tavares10@hotmail.com>
2022-09-28 10:45:43 -0300

    Random Fortune Service

--- spring-demo-annotation/src/com/luv2code/springdemo/RandomFortuneService.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,13 +1,24 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	private String[] fortunes;
+	private Random sorter;
+
+	public RandomFortuneService() {
+		this.sorter = new Random();
+		this.fortunes = new String[] { "Behind every great fortune lies a great crime.",
+				"Fortune favors the prepar ed mind.", "All things are difficult before they are easy." };
+	}
+
 	@Override
 	public String getFortune() {
-		return "Today is your random lucky day!";
+		return fortunes[this.sorter.nextInt(this.fortunes.length)];
 	}
 
 }
