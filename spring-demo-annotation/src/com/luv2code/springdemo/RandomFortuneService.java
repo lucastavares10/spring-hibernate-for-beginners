80ffbcc0adf56f6014e2ffe8b3a544674101743b
Lucas Tavares <lucas_tavares10@hotmail.com>
2022-10-05 22:40:16 -0300

    Practice Activity #5 - Dependency Injection with Annotations

--- spring-demo-annotation/src/com/luv2code/springdemo/RandomFortuneService.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -2,18 +2,19 @@ package com.luv2code.springdemo;
 
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	@Value("${foo.fortunes}")
 	private String[] fortunes;
+
 	private Random sorter;
 
 	public RandomFortuneService() {
 		this.sorter = new Random();
-		this.fortunes = new String[] { "Behind every great fortune lies a great crime.",
-				"Fortune favors the prepar ed mind.", "All things are difficult before they are easy." };
 	}
 
 	@Override
