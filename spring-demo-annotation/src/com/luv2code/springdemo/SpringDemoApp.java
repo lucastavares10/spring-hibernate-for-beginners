2868606bd0430c3aea1f5d9840b9051b29e71c3c
Lucas Tavares <lucas_tavares10@hotmail.com>
2022-10-05 22:06:16 -0300

    Inject properties file using Java annotations

--- spring-demo-annotation/src/com/luv2code/springdemo/SpringDemoApp.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/SpringDemoApp.java
@@ -8,11 +8,15 @@ public class SpringDemoApp {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
 		System.out.println(theCoach.getDailyWorkout());
 
 		System.out.println(theCoach.getDailyFortune());
+
+		System.out.println(theCoach.getEmail());
+
+		System.out.println(theCoach.getTeam());
 
 		context.close();
 
---
+++ spring-demo-annotation/src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwimCoach implements Coach {
+
+	@Value("${foo.email}")
+	private String email;
+
+	@Value("${foo.team}")
+	private String team;
+
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+
+	@Override
+	public String getDailyWorkout() {
+		return "Training within pool today...";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+}
